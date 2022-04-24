6008c8cedd0dd9ceb06f127f5d3facac3475e92a
PythonCAE <PythonCAE@gmail.com>
2022-04-24 18:48:35 +0545

    perfectNumber folder is added

---
+++ Perfect_number/Perfect_Number.py
@@ -0,0 +1,32 @@
+# num = int(input("Enter the Number:"))
+# sum=0
+
+#Using For Loop for the adding its factorial and adding into sum variable:
+# for i in range(1,num):
+#     if(num%i==0):
+#         sum=i+sum
+# if sum==num:
+#     print("yes")
+# else:
+#     print("no")    
+
+class Prefect_Number:
+    def __init__(self,num):
+        self.num=num
+
+    def check_Perfect_Number(self):
+        sum=0
+        for i in range(1,self.num) :
+            if(self.num%i==0):
+                sum=i+sum
+
+        if(sum==self.num):
+          print("yes")
+        else:
+         print("no") 
+num=   int(input("Enter the number:"))     
+perfnum= Prefect_Number(num)
+perfnum.check_Perfect_Number()
+   
+
+                           
---
+++ Perfect_number/ReadMe.md
@@ -0,0 +1,6 @@
+# Perect Number:
+perfect Number is a positive integer that is equal to the sum of its divisors excluding the number itself.
+
+## Example:
+6 = 1,2,3,6
+1+2+3=6
