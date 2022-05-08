2d666fdee278e5db2a64ae1871f82bd887b04c29
PythonCAE <PythonCAE@gmail.com>
2022-05-08 21:34:42 +0545

    While Loop is added

--- Sorting/Bubble_Sort/Bubble_sort_in_decending_Order.py
+++ Sorting/Bubble_Sort/Bubble_sort_in_decending_Order.py
@@ -1,3 +1,4 @@
+#Part 1
 List = []
 n =  int(input("Enter how many Number:"))
 for i in range(n):
@@ -8,5 +9,22 @@ for i in range(n,0,-1):
             List[j-1],List[j] = List[j],List[j-1]
 
 print(List)
-# a =()
-# print(a[0])
+
+
+#Part 2
+List = []
+n =  int(input("Enter how many Number:"))
+while(n>0):
+    List.append(int(input("Enter the Number:")))
+    n -= 1
+l = len(List)
+i = 0
+while(l>i):
+    j = l-1
+    k =0
+    while(j>k):
+        if List[j] > List[j-1]:
+            List[j-1],List[j] = List[j],List[j-1]        
+        j -= 1
+    i += 1
+print(List)               
