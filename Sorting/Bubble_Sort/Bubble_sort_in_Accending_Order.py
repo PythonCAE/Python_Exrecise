cb877f01e8d60ce873598024c540901afd81f055
PythonCAE <PythonCAE@gmail.com>
2022-05-08 21:33:32 +0545

    Using While_loop is also added

--- Sorting/Bubble_Sort/Bubble_sort_in_Accending_Order.py
+++ Sorting/Bubble_Sort/Bubble_sort_in_Accending_Order.py
@@ -1,3 +1,4 @@
+#Part 1
 List = [10,15,4,23,0]
 l = len(List)
 for i in range(l):
@@ -5,4 +6,17 @@ for i in range(l):
         if List[j]>List[j+1]:
             List[j],List[j+1] = List[j+1],List[j]
 
-print(List)            
+print(List)   
+
+#Part2
+List =[23,15,10,4,0]
+l = len(List)
+i = 0
+while(l>i):
+    j = 0
+    while(l-1>j):
+        if List[j]>List[j+1]:
+            List[j],List[j+1] = List[j+1],List[j]
+        j += 1
+    i += 1
+print(List)               
